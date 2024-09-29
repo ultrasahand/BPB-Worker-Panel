c4bf6acb06065709be8fe8c5a902154e9a072e43
ultrasahand <ultrasahand@yahoo.com>
2024-09-29 14:21:34 +0330

    Update _worker.js
    uuid

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '0500bdb2-66ed-4537-9d69-01ee946e6a78';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
